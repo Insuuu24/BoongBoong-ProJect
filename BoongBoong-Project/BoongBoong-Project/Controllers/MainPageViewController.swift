6ad122964a84709ed4c547072367daae680a504f
Insu <insu2405@gmail.com>
2023-09-05 04:05:51 +0900

    [Feat]: MainPage 기본 구현

--- BoongBoong-Project/BoongBoong-Project/Controllers/MainPageViewController.swift
+++ BoongBoong-Project/BoongBoong-Project/Controllers/MainPageViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import MapKit
 
 class MainPageViewController: UIViewController {
 
+    @IBOutlet weak var homeMap: MKMapView!
+    @IBOutlet weak var mapSearchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
