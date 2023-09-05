9c1cbf21d35861231b515347446f800883d3401a
LeeJaeheee <sns3434@gmail.com>
2023-09-05 14:53:14 +0900

    [Feat]: 지도 권한 설정 및 현위치로 이동 구현

--- BoongBoong-Project/BoongBoong-Project/Controllers/AddKickBoardViewController.swift
+++ BoongBoong-Project/BoongBoong-Project/Controllers/AddKickBoardViewController.swift
@@ -6,24 +6,56 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class AddKickBoardViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var kickboardMapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    // TODO: 위치 권한 허용 안함 선택했을 시 다시 요청
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        kickboardMapView.showsUserLocation = true
     }
-    */
+    
+    @IBAction func regionButtonTapped(_ sender: UIButton) {
+        if let userLocation = kickboardMapView.userLocation.location {
+            let region = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 300, longitudinalMeters: 300)
+            kickboardMapView.setRegion(region, animated: true)
+        }
+    }
+    
+    
+}
 
+// MARK: - CLLocationManagerDelegate
+
+extension AddKickBoardViewController: CLLocationManagerDelegate {
+    
+    // 위치 업데이트 시 호출되는 메서드
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let regionRadius: CLLocationDistance = 300 // 300m
+            
+            let region = MKCoordinateRegion(center: center, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+            kickboardMapView.setRegion(region, animated: true)
+            locationManager.stopUpdatingLocation()
+        }
+    }
 }
